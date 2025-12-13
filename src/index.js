0f5d5ced797583b367670f080fc648874f11b97d
Ivan Yelizariev <info@odoomagic.com>
2025-12-13 13:50:33 +0100

    fix-fix

--- src/index.js
+++ src/index.js
@@ -15,16 +15,9 @@ function Root() {
     <>
       <App />
       <div style={{ position: 'absolute', pointerEvents: 'none', top: 0, left: 0, width: '100vw', height: '100vh' }}>
-        <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-          pmnd.rs
-          <br />
-          dev collective
-        </a>
-        <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>18/01/2024</div>
-        <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
-        <a style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }} href="#">
-          scroll up/down ...
-        </a>
+         <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}><a href={window.APP_CONFIG.Next}>*.mydream42.com</a></div>
+        <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>nenaneffati.com</div>
+        <div style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>beer.mydream42.com</div>
       </div>{' '}
     </>
   )
